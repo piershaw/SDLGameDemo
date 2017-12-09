6474fa7ed26d129c504259fcd4a70d64aa77ae2e
Pier Shaw <piershaw@gmail.com>
2017-12-08 22:01:00 -0800

    [works on both ide]

--- Game/Game/Game.cpp
+++ Game/Game/Game.cpp
@@ -1,15 +1,12 @@
 // for windows
-//#include "stdsfx.h"
-
 #include "stdio.h"
 #include "stdlib.h"
 
-//using namespace std;
+//#ifdef _WIN32 // well just for vs
+#include "stdafx.h"
+//#endif
 
 int main(){
-	//std::cout << "hello //world" << std::endl;
-	
 	printf("hello world");
-	
 	return 0;
 }
